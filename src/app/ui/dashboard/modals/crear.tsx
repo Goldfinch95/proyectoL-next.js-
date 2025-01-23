fadb7d998f57485f86cbebbc34adcebbf4ac1def
Goldfinch <facu_vila@hotmail.com>
2025-01-23 18:35:23 -0300

    se trabajo en la logica del modal crear cuenta

--- src/app/ui/dashboard/modals/crear.tsx
+++ src/app/ui/dashboard/modals/crear.tsx
@@ -1,8 +1,43 @@
+"use client"
+
+import { useState, useEffect } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Confirm from "./confirm";
 
 
 export default function Crear() {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value); 
+    console.log(event.target.value); 
+  };
+
+  const resetInputValue = () => {
+    setTimeout(() => {
+      setInputValue(''); 
+    }, 1000); 
+  };
+
+  useEffect(() => {
+    const modalElement = document.getElementById('modalCrear');
+    const handleModalClose = () => {
+      resetInputValue(); 
+    };
+
+    
+    if (modalElement) {
+      modalElement.addEventListener('hide.bs.modal', handleModalClose);
+    }
+
+    
+    return () => {
+      if (modalElement) {
+        modalElement.removeEventListener('hide.bs.modal', handleModalClose);
+      }
+    };
+  }, []);
+
   return (
     <div
       className="modal fade"
@@ -16,7 +51,6 @@ export default function Crear() {
           className="modal-content modal-costum p-2 pt-0"
           style={{ backgroundColor: "#FAF9F5" }}
         >
-          {/* Header del Modal */}
           <div className="modal-header border-0">
             <h5 className="modal-title align-self-end pt-3 mb-0">
               Crear Cuenta
@@ -28,24 +62,23 @@ export default function Crear() {
               aria-label="Close"
             ></button>
           </div>
-
-          {/* Body del Modal */}
           <div className="modal-body d-flex flex-column">
             <input
               
               type="text"
+              value={inputValue}
+              onChange={handleInputChange}
               placeholder="Item"
               className="ps-4 py-2 rounded-3 border-1 border-dark"
               style={{ backgroundColor: "#fff" }}
             />
           </div>
-
-          {/* Footer del Modal */}
           <div className="modal-footer d-flex flex-nowrap justify-content-between w-100 border-0">
             <button
               type="button"
               className="btn btn-light border-2 border-dark rounded-pill px-4 w-100"
               data-bs-dismiss="modal"
+              onClick={resetInputValue}
             >
               Cancelar
             </button>
